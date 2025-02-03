488e3e9603c210ef52cee2e67c06b804c095ee8a
Nathan Marcus <n-marcus@users.noreply.github.com>
2025-02-03 11:47:02 +0100

    Debug info in LED state

--- Internal_LED_and_Touch/LED.ino
+++ Internal_LED_and_Touch/LED.ino
@@ -30,27 +30,32 @@ void setupLED() {
 }
 
 void updateLED() {
-  
+
   //LAURA, here you can change the BPMS that will trigger a certain state
+  String state = "neutral";
   int heartbeatRate = receivedData.heartbeatRate;
-  if (heartbeatRate > 80 && heartbeatRate < 100) { 
+  if (heartbeatRate > 80 && heartbeatRate < 100) {
 
     //this is the excited state
     excitedState = true;
     stressedState = false;
-  } else if (heartbeatRate > 100) { 
+    state = "excited";
+  } else if (heartbeatRate > 100) {
     //this is the stressed state
     excitedState = false;
     stressedState = true;
-  } else { 
+    state = "stressed";
+  } else {
     //this is default state
     excitedState = false;
     stressedState = false;
   }
 
 
-  if (humanPresence) {
-      //show the led when there seems to be someone close to the sensor
+  Serial.println("State is " + String(state));
+
+    if (humanPresence) {
+    //show the led when there seems to be someone close to the sensor
     digitalWrite(LED_BUILTIN, HIGH);
     fadeInPercentage = constrain(fadeInPercentage + 0.01, 0.0, 1.0);
     // Serial.println("FadeInPercentage:" + String(fadeInPercentage));
@@ -75,6 +80,9 @@ void updateLED() {
       //make the hue change over time
       hue += (breathCycleSineWaveLED * hueRange);
 
+      //make sure it wraps back to 0 if it overflows
+      hue = hue % 1.0;
+
       //convert the floating hue to a hue value on the hue wheel
       LEDHue = int(float(hueWheelSize) * hue);
 
@@ -83,7 +91,7 @@ void updateLED() {
 
       //slowly fade the LEDs in when we detect a human again
       // LEDBrightness = LEDBrightness * fadeInPercentage;
-      
+
 
       //make sure the LEDs never fully turn off by setting a minimum of 10 for the brightness
       LEDBrightness = constrain(LEDBrightness, LEDminimumBrightness, 255);
@@ -95,13 +103,16 @@ void updateLED() {
       strip.fill(rgbcolor);
       strip.show();  //  Update strip to match
     }
-  } else {
+
+    Serial.println("Hue is " + String(hue));
+  }
+  else {
     digitalWrite(LED_BUILTIN, LOW);
     //slowly fade the LED strip out
     LEDBrightness -= 0.1;
     LEDBrightness = constrain(LEDBrightness, 0, 255);
     uint32_t rgbcolor = strip.ColorHSV(LEDHue, saturation, LEDBrightness);
-    fadeInPercentage = 0.0; //make sure we fade in when we get data next time
+    fadeInPercentage = 0.0;  //make sure we fade in when we get data next time
 
     strip.fill(rgbcolor);
     strip.show();
